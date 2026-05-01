77ceba58d9f8f84d6998b163f1318a3b265743da
Hashmeet S. <103609995+hashmeet02@users.noreply.github.com>
2026-05-01 20:23:05 +0000

    fix: resume fix

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -16,7 +16,7 @@ const Resume = () => {
   <div
     className='absolute inset-0 flex items-center justify-center z-10'
   >
-    <a href="hashmeet_singh_saini_resume.docx" target="_blank" className='flex items-center justify-center'>
+    <a href="/hashmeet_saini_resume.docx" target="_blank" className='flex items-center justify-center'>
     <div
       className=' bg-blue-500/50  backdrop-blur-md rounded-full w-48 h-48 md:w-72 md:h-72 flex items-center justify-center transition transform hover:scale-150 duration-700 '
     >
